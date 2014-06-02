21aba4409817da1664d2f7027118ad63b99493f5
wick-ed <b.wick@techdivision.com>
2014-06-02 18:41:26 +0200

    initial commit

---
+++ tests/TechDivision/Markman/MarkmanTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category  Tools
+ * @package   Markman
+ * @author    Bernhard Wick <b.wick@techdivision.com>
+ * @copyright 2014 TechDivision GmbH - <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php
+ *            Open Software License (OSL 3.0)
+ * @link      http://www.techdivision.com/
+ */
+
+namespace TechDivision\Markman;
+
+/**
+ * TechDivision\Markman\MarkmanTest
+ *
+ * Basic test class for the Markman library.
+ *
+ * @category  Tools
+ * @package   Markman
+ * @author    Bernhard Wick <b.wick@techdivision.com>
+ * @copyright 2014 TechDivision GmbH - <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php
+ *            Open Software License (OSL 3.0)
+ * @link      http://www.techdivision.com/
+ */
+class MarkmanTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test if the constructor created an instance of the rewrite module.
+     *
+     * @return void
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf('\TechDivision\RewriteModule\RewriteModule', $this->rewriteModule);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+$loader = require '${php-target.dir}/vendor/autoload.php';
+$loader->add('TechDivision\\Markman\\', '${php-target.dir}/vendor/techdivision/markman/src');
