7a65d21a037f22f3784c579ed61b7dd2abc91841
Lars Röttig <l.roettig@techdivision.com>
2014-06-16 14:54:04 +0200

    doc style

--- src/templates/appserver.io/library/lib-config.js
+++ src/templates/appserver.io/library/lib-config.js
@@ -2,13 +2,13 @@
  *
  * JavaScript Library Configuration
  *
- * @category   Appserver
- * @package    TechDivision_Markman
- * @subpackage Utils
- * @author     Lars Roettig <l.roettig@techdsivision.com>
- * @copyright  2014 TechDivision GmbH - <info@techdivision.com>
- * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link       http://www.techdivision.com/
+ * @category  Appserver
+ * @package   TechDivision_Markman
+ *
+ * @author    Lars Roettig <l.roettig@techdsivision.com>
+ * @copyright 2014 TechDivision GmbH - <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.techdivision.com/
  */
 $(document).ready(function () {
     var menu = $('#menu');
@@ -24,7 +24,7 @@ $(document).ready(function () {
                 options = arguments[3],
                 itemhref = $item.find('a:first').attr("href");
              /** @todo can be remove if working the navigation*/
-            itemhref = 'http://localhost:9080/docs/appserver.io/0.6.0-beta1/docs/getting-started/basic-usage.html';
+
             $('#documentation').load(itemhref + ' #documentation');
         }
     });
